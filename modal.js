691ea6f57be21d984f389bab42e0965804dff2a3
ldoba <lisadobarro@yahoo.fr>
2020-09-10 19:23:57 +0200

    MODAL OK

--- modal.js
+++ modal.js
@@ -1,24 +1,23 @@
 
 let modal = null;
 
-const openModal = function(e){
+const openModal = function(e) {
     e.preventDefault();
-    console.log('fuck');
     const target = document.querySelector(e.target.getAttribute('href'));
     target.style.display = null;
     target.removeAttribute('aria-hidden');
-    target.setAttribute('aria-modal', true);
+    target.setAttribute('aria-modal', 'true');
     modal = target;
     modal.addEventListener('click', closeModal);
-    modal.querySelector('.js-modal-close').addEventListener('click', closeModal); 
-    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation); 
+    modal.querySelector('.js-modal-close').addEventListener('click', closeModal);
+    modal.querySelector('.js-modal-stop').addEventListener('click', stopPropagation);
 
 }
 const closeModal = function(e){
     if (modal === null) return
     e.preventDefault();
     modal.style.display = "none";
-    modal.setAttribute('aria-hidden', true);
+    modal.setAttribute('aria-hidden', 'true');
     modal.removeAttribute('aria-modal');
     modal.removeEventListener('click', closeModal);
     modal.querySelector('.js-modal-close').removeEventListener('click', closeModal);
